37551847f5388b5d1f5ae014fceb0d8acd4f0966
Alex Otero <152084340+alexoterol@users.noreply.github.com>
2025-06-08 22:54:55 +0000

    Añadir sección de Localización  y crowd meter. Corrección de estilo.

---
+++ src/js/toggle.js
@@ -0,0 +1,34 @@
+function toggleVista(id) {
+  const mapa = document.getElementById(`mapa-${id}`);
+  const crowd = document.getElementById(`crowdmeter-${id}`);
+  mapa.classList.toggle('hidden');
+  crowd.classList.toggle('hidden');
+}
+
+// Carrusel simple con botones
+let slideIndex = 0;
+const carousel = document.getElementById('carousel');
+const slides = document.querySelectorAll('#carousel > div');
+
+function updateCarousel() {
+  const slideWidth = carousel.offsetWidth;
+  carousel.scrollTo({
+    left: slideWidth * slideIndex,
+    behavior: 'smooth'
+  });
+}
+
+
+document.getElementById('prevSlide').addEventListener('click', () => {
+  if (slideIndex > 0) {
+    slideIndex--;
+    updateCarousel();
+  }
+});
+
+document.getElementById('nextSlide').addEventListener('click', () => {
+  if (slideIndex < slides.length - 1) {
+    slideIndex++;
+    updateCarousel();
+  }
+});
