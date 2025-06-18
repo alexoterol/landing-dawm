da7bddbd9be0b64e11c7473abd845d08d5df9750
Alex Otero <152084340+alexoterol@users.noreply.github.com>
2025-06-18 23:55:52 +0000

    Correccion carrucel

--- public/src/js/file01.js
+++ public/src/js/file01.js
@@ -378,6 +378,33 @@ ubicaciones.forEach((ubicacion) => {
 
 
 
+// Carrusel simple con botones
+let slideIndex = 0;
+const slides = () => document.querySelectorAll('#carousel > div');
+
+function updateCarousel() {
+  const slideWidth = carousel.offsetWidth;
+  carousel.scrollTo({
+    left: slideWidth * slideIndex,
+    behavior: 'smooth'
+  });
+}
+
+document.getElementById('prevSlide').addEventListener('click', () => {
+  if (slideIndex > 0) {
+    slideIndex--;
+    updateCarousel();
+  }
+});
+
+document.getElementById('nextSlide').addEventListener('click', () => {
+  if (slideIndex < slides().length - 1) {
+    slideIndex++;
+    updateCarousel();
+  }
+});
+
+
 (() => {
     showToast();
     showVideo();
--- public/src/js/toggle.js
+++ public/src/js/toggle.js
@@ -9,28 +9,3 @@ function toggleVista(id) {
   dot.classList.toggle("opacity-100", !isMapVisible);
   dot.classList.toggle("opacity-40", isMapVisible);
 }
-// Carrusel simple con botones
-let slideIndex = 0;
-const slides = () => document.querySelectorAll('#carousel > div');
-
-function updateCarousel() {
-  const slideWidth = carousel.offsetWidth;
-  carousel.scrollTo({
-    left: slideWidth * slideIndex,
-    behavior: 'smooth'
-  });
-}
-
-document.getElementById('prevSlide').addEventListener('click', () => {
-  if (slideIndex > 0) {
-    slideIndex--;
-    updateCarousel();
-  }
-});
-
-document.getElementById('nextSlide').addEventListener('click', () => {
-  if (slideIndex < slides().length - 1) {
-    slideIndex++;
-    updateCarousel();
-  }
-});
